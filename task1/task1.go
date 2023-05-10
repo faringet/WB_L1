ca2a3c302497c0ec8799191071f54dac1622c181
faringet <mik.davydov14@gmail.com>
2023-05-10 16:07:35 +0300

    task1 added

---
+++ task1/task1.go
@@ -0,0 +1,67 @@
+/*
+Дана структура Human (с произвольным набором полей и методов).
+Реализовать встраивание методов в структуре Action от родительской структуры
+Human (аналог наследования).
+*/
+
+package main
+
+import "fmt"
+
+type Human struct {
+	Age  int
+	Name string
+	Sex  string
+}
+
+// Метод структуры Human
+func (h *Human) getHumanAge() {
+	fmt.Println(h.Age)
+}
+
+// Метод структуры Human
+func (h *Human) getHumanName() {
+	fmt.Println(h.Name)
+}
+
+// Метод структуры Human
+func (h *Human) getHumanSex() {
+	fmt.Println(h.Sex)
+}
+
+// Print Метод структуры Human
+func (h *Human) Print() {
+	fmt.Println("parent")
+}
+
+// Встраивание Human в Action
+type Action struct {
+	Human
+}
+
+// Print Метод структуры Action
+func (a *Action) Print() {
+	fmt.Println("child")
+}
+
+func main() {
+	// Переменные Боб и Алиса типа Action
+	Bob := Action{Human{Age: 30, Name: "Bob", Sex: "male"}}
+	Alice := Action{Human{Age: 25, Name: "Alice", Sex: "female"}}
+
+	// Метод структуры Human работает на переменной типа Action
+	Bob.getHumanAge()
+	Bob.getHumanName()
+	Bob.getHumanSex()
+
+	// Метод структуры Human работает на переменной типа Action
+	Alice.getHumanAge()
+	Alice.getHumanName()
+	Alice.getHumanSex()
+
+	// + бонус - Что будет, если и в родительской и дочерней структуре есть реализация методов с одинаковым названием?
+	var x Action
+	// Реализация родительского метода будет переписана реализацией дочернего метода
+	x.Print()
+
+}
