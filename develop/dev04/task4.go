704f8bad20187997a73d7423a445b831fadb85bf
faringet <mik.davydov14@gmail.com>
2023-05-11 12:52:57 +0300

    task4 updated

--- develop/dev04/task4.go
+++ develop/dev04/task4.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
 /*
@@ -10,6 +13,7 @@ import (
 набор из N воркеров, которые читают произвольные данные из канала и
 выводят в stdout. Необходима возможность выбора количества воркеров при
 старте.
+Программа должна завершаться по нажатию Ctrl+C.
 */
 
 func main() {
@@ -18,39 +22,61 @@ func main() {
 	fmt.Print("Введите количество воркеров: ")
 	fmt.Scanln(&numWorkers)
 
-	// Создаем канал для передачи данных
+	// Канал для передачи данных
 	dataChan := make(chan int)
 
-	// Создаем WaitGroup для ожидания завершения всех воркеров
+	// Канал для получения сигнала завершения
+	doneChan := make(chan struct{})
+
+	// WaitGroup для ожидания завершения всех воркеров
 	var wg sync.WaitGroup
 	wg.Add(numWorkers)
 
-	// Запускаем воркеров
+	// Запуск воркеров
 	for i := 0; i < numWorkers; i++ {
-		go worker(i+1, dataChan, &wg)
+		go worker(i+1, dataChan, &wg, doneChan)
 	}
 
-	// Отправляем данные (int от 1 до кол-ва, который юзер ввел) в канал
+	// Отправка данных (int от 1 до кол-ва, который юзер ввел) в канал
 	for i := 1; i <= numWorkers; i++ {
 		dataChan <- i
 	}
 
+	// Канал, который ловит сигналы ОС
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Ожидание когда юзер завершит программу (Ctrl+C)
+	<-sigChan
+
 	// Закрываем канал, чтобы воркеры могли завершиться
 	close(dataChan)
+	close(doneChan)
 
 	// Ожидаем завершения всех воркеров
 	wg.Wait()
 
-	fmt.Println("Главный поток завершился")
+	fmt.Println("main завершился")
 }
 
-func worker(workerID int, dataChan <-chan int, wg *sync.WaitGroup) {
+func worker(workerID int, dataChan <-chan int, wg *sync.WaitGroup, doneChan <-chan struct{}) {
 	defer wg.Done()
 
-	// Читаем данные из канала до его закрытия
-	for data := range dataChan {
-		fmt.Printf("Воркер %d: %d\n", workerID, data)
-	}
+	// Чтение данных из канала до его закрытия || получения сигнала завершения
+	for {
+		select {
+		case data, ok := <-dataChan:
+			if !ok {
+				// Канал данных закрыт, завершение работы
+				fmt.Printf("Воркер %d завершился\n", workerID)
+				return
+			}
+			fmt.Printf("Воркер %d: %d\n", workerID, data)
 
-	fmt.Printf("Воркер %d завершился\n", workerID)
+		case <-doneChan:
+			// Получен сигнал завершения программы, завершаем работу воркера
+			fmt.Printf("Воркер %d завершился\n", workerID)
+			return
+		}
+	}
 }
